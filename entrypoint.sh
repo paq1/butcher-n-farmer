63c8fd694779a7e37cd31d2b99c6d0f5308e3de5
paq1 <p.paquin1605@gmail.com>
2025-01-12 18:23:47 +0100

    ci

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "Starting entrypoint script..."
+
+# Debug : Vérifie si les variables d'environnement sont présentes
+echo "Environment variables:"
+env
+
+# Chemin du fichier env-config.js
+ENV_CONFIG_FILE="/usr/share/nginx/html/assets/env-config.js"
+echo "Writing environment variables to $ENV_CONFIG_FILE"
+
+# Remplacement des placeholders par les valeurs des variables d'environnement
+cat <<EOF > $ENV_CONFIG_FILE
+(function (window) {
+  window.__env = window.__env || {};
+  window.__env.ENV = "${ENV}";
+})(this);
+EOF
+
+# Debug : Vérifie si le fichier a été généré correctement
+echo "Generated $ENV_CONFIG_FILE:"
+cat $ENV_CONFIG_FILE
+
+# Vérifie la configuration NGINX avant de démarrer
+echo "Checking NGINX configuration..."
+nginx -t || exit 1
+
+# Démarre NGINX en mode "foreground"
+echo "Starting NGINX..."
+nginx -g "daemon off;"
---
+++ src/assets/env-config.js
@@ -0,0 +1,4 @@
+(function (window) {
+  window.__env = window.__env || {};
+  window.__env.ENV = window.__env.ENV || "local";
+})(this);
